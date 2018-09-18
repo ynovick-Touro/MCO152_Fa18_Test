da5d676e323dc25ebe22f09a21e032b3726a4921
Yitzchak Novick <ynovick@admin.touro.edu>
2018-09-17 21:17:15 -0400

    HelloWorld uses message generator

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		String msg = MessageGenerator.getMsg();
+		System.out.println(msg);
 	}
 
 }
