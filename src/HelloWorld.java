ae147a7a91fd86971c1286a9f9fd7cc191009257
ynovick-Touro <yitzchak.novick@touro.edu>
2018-10-03 21:12:41 -0400

    Appended msg notice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String msg = MessageGenerator.getMsg();
+		String msg = MessageGenerator.getMsg() + " - message";
 		System.out.println(msg);
 	}
 
